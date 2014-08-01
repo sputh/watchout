81130d7e8cfc1e5270648afa28be7c05d27c27ba
sputh <samantha.puth@gmail.com>
2014-08-01 15:49:01 -0700

    FIXED enemies in an SVG element

--- watchout.js
+++ watchout.js
@@ -2,10 +2,13 @@
 
 var dataset = [1, 2, 3, 4, 5];
 
-d3.select(".container").selectAll("svg")
+d3.select(".container").selectAll("circle")
   .data(dataset)
   .enter()
-  .append("svg")
-  .attr("class", "enemies");
+  .append("circle")
+  .attr("class", "enemies")
+  .attr("cx", "50")
+  .attr("cy", "50")
+  .attr("r", "50");
   //.style("width", "50px")
   // .style("height", "50px");
