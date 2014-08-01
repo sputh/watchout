f79a18d5c0b34378407574885b415a9f5116a190
sputh <samantha.puth@gmail.com>
2014-08-01 15:33:20 -0700

    created enemies in an SVG element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,11 @@
 // start slingin' some d3 here.
 
+var dataset = [1, 2, 3, 4, 5];
+
+d3.select(".container").selectAll("svg")
+  .data(dataset)
+  .enter()
+  .append("svg")
+  .attr("class", "enemies");
+  //.style("width", "50px")
+  // .style("height", "50px");
